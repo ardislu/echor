54d831866e8370cbcf03503ed64555af99c7fec1
Ardis Lu <ardisglu@gmail.com>
2023-02-13 21:07:55 -0800

    Rename argument to "string"
    Matches man echo

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// The text to echo
-    text: Option<String>,
+    /// The string to display
+    string: Option<String>,
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    if let Some(string) = cli.text.as_deref() {
+    if let Some(string) = cli.string.as_deref() {
         println!("{string}");
     }
 }
